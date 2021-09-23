4639f6d0db94b65d85a2198cd7691b5822e805d6
bobby_2075 <mkaa2002@gmail.com>
2021-09-23 13:37:12 +0200

    lmao

--- src/js/slideshow.js
+++ src/js/slideshow.js
@@ -1,16 +1,15 @@
 var slideIndex = 0;
-showSlide();
-
-function showSlide() {
+showSlides();
+function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("slideshow");
-
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var slides = document.getElementsByClassName('image');
+    for (var i_1 = 0; i_1 < slides.length; i_1++) {
+        slides[i_1].style.display = "none";
     }
-
     slideIndex++;
-    if(slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlide, 2000);
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
 }
